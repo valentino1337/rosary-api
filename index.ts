1970e5e4e7cb79c1b30bcedcacbae906d04d8474
Richard Jevčák <cordon.tuque-0k@icloud.com>
2023-10-14 08:07:07 +0000

    backup

--- index.ts
+++ index.ts
@@ -9,26 +9,53 @@ const server = Bun.serve({
   fetch(request: Request) {
     if (request.method === "GET") {
       if (request.url.endsWith("/api")){//klasicky request
-        if (dayOfWeek === 0) {
-          return new Response('{ "status": "success", "day": "Sunday", rosary: "Glorious"}', {status:200})
-        }
-        else if (dayOfWeek === 1) {
-          return new Response('{ "status": "success", "day": "Monday", "rosary": "Joyful" }', {status:200})
-        }
-        else if (dayOfWeek === 2) {
-          return new Response('{ "status": "success", "day": "Tuesday", "rosary": "Sorrowful" }', {status:200})
-        }
-        else if (dayOfWeek === 3) {
-          return new Response('{ "status": "success", "day": "Wednesday", "rosary": "Glorious" }', {status:200})
-        }
-        else if (dayOfWeek === 4) {
-          return new Response('{ "status": "success", "day": "Thursday", "rosary": "Luminous" }', {status:200})
-        }
-        else if (dayOfWeek === 5) {
-          return new Response('{ "status": "success", "day": "Friday", "rosary": "Sorrowful" }', {status:200})
-        }
-        else if (dayOfWeek === 6) {
-          return new Response('{ "status": "success", "day": "Saturday", "rosary": "Joyful" }', {status:200})
+
+        if (request.url.endsWith('/api/mysteries')){// AJ TAJOMSTVA ///TODO este dat aj /api/mysteries/joyful atd
+          if (dayOfWeek === 0) {
+            return new Response('{ "status": "success", "day": "Sunday", rosary: "Glorious"}', {status:200})
+          }
+          else if (dayOfWeek === 1) {
+            return new Response('{ "status": "success", "day": "Monday", "rosary": "Joyful" }', {status:200})
+          }
+          else if (dayOfWeek === 2) {
+            return new Response('{ "status": "success", "day": "Tuesday", "rosary": "Sorrowful" }', {status:200})
+          }
+          else if (dayOfWeek === 3) {
+            return new Response('{ "status": "success", "day": "Wednesday", "rosary": "Glorious" }', {status:200})
+          }
+          else if (dayOfWeek === 4) {
+            return new Response('{ "status": "success", "day": "Thursday", "rosary": "Luminous" }', {status:200})
+          }
+          else if (dayOfWeek === 5) {
+            return new Response('{ "status": "success", "day": "Friday", "rosary": "Sorrowful" }', {status:200})
+          }
+          else if (dayOfWeek === 6) {
+            return new Response('{ "status": "success", "day": "Saturday", "rosary": "Joyful" }', {status:200})
+          }
+
+        } //IBA AKY RUZENEC SA MODLI
+        else {
+          if (dayOfWeek === 0) {
+            return new Response('{ "status": "success", "day": "Sunday", rosary: "Glorious"}', {status:200})
+          }
+          else if (dayOfWeek === 1) {
+            return new Response('{ "status": "success", "day": "Monday", "rosary": "Joyful" }', {status:200})
+          }
+          else if (dayOfWeek === 2) {
+            return new Response('{ "status": "success", "day": "Tuesday", "rosary": "Sorrowful" }', {status:200})
+          }
+          else if (dayOfWeek === 3) {
+            return new Response('{ "status": "success", "day": "Wednesday", "rosary": "Glorious" }', {status:200})
+          }
+          else if (dayOfWeek === 4) {
+            return new Response('{ "status": "success", "day": "Thursday", "rosary": "Luminous" }', {status:200})
+          }
+          else if (dayOfWeek === 5) {
+            return new Response('{ "status": "success", "day": "Friday", "rosary": "Sorrowful" }', {status:200})
+          }
+          else if (dayOfWeek === 6) {
+            return new Response('{ "status": "success", "day": "Saturday", "rosary": "Joyful" }', {status:200})
+          }
         }
       }
       else { return new Response('Not found', {status: 404})}
